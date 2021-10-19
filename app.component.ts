9ce53c16b754d9935b77d7d1f768e0324022a170
LoknathSurabhi <loknath_201900374@smit.smu.edu.in>
2021-10-19 21:37:57 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'To-Do List';
+  list:any[]=[];
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item});
+    console.warn(this.list);
+  }
+  removeTask (id:number)
+
+{
+
+console.warn(id)
+
+this.list=this.list.filter(item=>item.id!==id)
+}
+}
